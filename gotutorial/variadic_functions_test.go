6a7fdd0dd6ebf9d16fc08bdace2eb3009c0b6482
AndiHappy <1015126672@qq.com>
2024-01-18 14:37:01 +0000

    tt

--- gotutorial/variadic_functions_test.go
+++ gotutorial/variadic_functions_test.go
@@ -17,6 +17,30 @@ func variadicExample(s ...string) {
 
 }
 
+func variadicExample2(s ...string) {
+	fmt.Println(s)
+}
+
+func calculation(str string, y ...int) int {
+	area := 1
+	for _, val := range y {
+		if str == "Rectangle" {
+			area *= val
+		} else if str == "Square" {
+			area = val * val
+		}
+	}
+	return area
+}
+
 func TestVariablesFunction(t *testing.T) {
 	variadicExample("hell0", "world", "ddd")
+
+	variadicExample2()
+	variadicExample2("red", "blue")
+	variadicExample2("red", "blue", "green")
+	variadicExample2("red", "blue", "green", "yellow")
+
+	fmt.Println(calculation("Rectangle", 20, 30))
+	fmt.Println(calculation("Square", 20))
 }
