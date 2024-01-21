1ee54f2a6a53e2682c3660ff3579c38d70095010
AndiHappy <1015126672@qq.com>
2024-01-21 01:37:44 +0000

    test switch case

--- gotutorial/flow_control_test.go
+++ gotutorial/flow_control_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestFlowControl(t *testing.T) {
@@ -96,5 +97,83 @@ func TestRangeLoopOverString(t *testing.T) {
 
 	for v, c := range "Hello" {
 		fmt.Println(v, c, string(c), "Hello")
+	}
+}
+
+func TestSwitch(t *testing.T) {
+	today := time.Now()
+	switch today.Day() {
+	case 5:
+		fmt.Println("Today is 5th. Clean your house.")
+	case 10:
+		fmt.Println("Today is 10th. Buy some wine.")
+	case 15:
+		fmt.Println("Today is 15th. Visit a doctor.")
+	case 25:
+		fmt.Println("Today is 25th. Buy some food.")
+	case 31:
+		fmt.Println("Party tonight.")
+	default:
+		fmt.Println("No information available for that day.")
+	}
+
+	var t1 int = today.Day()
+	switch t1 {
+	case 5, 10, 15:
+		fmt.Println("Clean your house.")
+	case 25, 26, 27:
+		fmt.Println("Buy some food.")
+	case 31:
+		fmt.Println("Party tonight.")
+	default:
+		fmt.Println("No information available for that day.")
+	}
+
+	switch today.Day() {
+	case 5:
+		fmt.Println("Clean your house.")
+		fallthrough
+	case 10:
+		fmt.Println("Buy some wine.")
+		fallthrough
+	case 15:
+		fmt.Println("Visit a doctor.")
+		fallthrough
+	case 21:
+		fmt.Println("Work Harder Boy.")
+		fallthrough
+	case 25:
+		fmt.Println("Buy some food.")
+		fallthrough
+	case 31:
+		fmt.Println("Party tonight.")
+	default:
+		fmt.Println("No information available for that day.")
+	}
+
+	switch {
+	case today.Day() < 5:
+		fmt.Println("Clean your house.")
+	case today.Day() <= 10:
+		fmt.Println("Buy some wine.")
+	case today.Day() > 15:
+		fmt.Println("Visit a doctor.")
+	case today.Day() == 25:
+		fmt.Println("Buy some food.")
+	default:
+		fmt.Println("No information available for that day.")
+	}
+
+	switch today := time.Now(); {
+	case today.Day() < 5:
+		fmt.Println("Clean your house.")
+	case today.Day() <= 10:
+		fmt.Println("Buy some wine.")
+	case today.Day() > 15:
+		fmt.Println("Visit a doctor.")
+	case today.Day() == 25:
+		fmt.Println("Buy some food.")
+	default:
+		fmt.Println("No information available for that day.")
 	}
 }
